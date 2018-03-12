45221283b5c6613454b120b581e777cd0d4b53c0
qiuXin123 <467516046@qq.com>
2018-03-12 16:23:56 +0800

    Initial commit

---
+++ node/node.js
@@ -0,0 +1,14 @@
+/**
+ * Created by surface on 2018/3/1.
+ */
+var http = required("http");
+http.createServer(function(request,response){
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8000);
+console.log('Server running at http://127.0.0.1:8888/');
